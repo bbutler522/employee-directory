4e144dabe43f91af3af35ab440cd3f7b1430153b
Brennan Butler <bbutler@newshour.org>
2021-10-29 16:54:20 -0400

    Add initial test with jest

---
+++ api/index.test.ts
@@ -0,0 +1,32 @@
+// import { KeystoneContext } from '@keystone-next/keystone/types';
+import { setupTestEnv, setupTestRunner, TestEnv } from '@keystone-next/keystone/testing';
+import { lists } from './schema';
+
+
+let config = {
+  db: {
+    provider: 'sqlite',
+    url: 'file:./keystone.db',
+    async onConnect(context) {
+      if (process.argv.includes('--seed-data')) {
+        await insertSeedData(context);
+      }
+    },
+  },
+  lists,
+}
+
+const runner = setupTestRunner({ config });
+
+test(
+  'Employee Create',
+  runner(async ({ context }) => {
+    const employee = await context.query.Employee.createOne({
+      data: { firstName: 'Allison', lastName: 'Wonderland', slug: 'AWonderland', email: 'AWonderland@example.com'},
+      query: 'id firstName lastName slug email',
+    });
+    expect(employee.firstName).toEqual('Allison');
+    expect(employee.email).toEqual('AWonderland@example.com');
+  })
+);
+
